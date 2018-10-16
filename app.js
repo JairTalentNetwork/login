8e3b763b805c6a7b258a5e50dd52f6af815e34df
Jair <jairanpo@gmail.com>
2018-10-16 11:58:12 -0500

    Signin validate if user exist on database

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+const authRoutes = require('./Routes/auth');
+
 // Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/login', express.static(
   __dirname  + '/public/authentication'));
 
 // Routes
-app.use('/login', require('./Routes/auth'));
+app.use('/login', authRoutes)
 
 // Starting the server
 const port = process.env.PORT || 3000;
